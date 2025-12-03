69dbdd77d01fe0ee6572bd4ead191f77351744f9
Aravind <gmarav005@gmail.com>
2025-12-03 19:29:00 +0530

    Add styles

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,13 @@ export default function AssemblyEndgame() {
         <main>
             <header>
                 <h1>Assembly: Endgame</h1>
-                <p>Guess the word within 8 attempts to keep the 
+                <p>Guess the word within 8 attempts to keep the
                 programming world safe from Assembly!</p>
             </header>
+            <section className="game-status">
+                <h2>You win!</h2>
+                <p>Well done! 🎉</p>
+            </section>
         </main>
     )
 }
